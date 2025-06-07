1653bb460ac2130231347a1d6fbe2f14fc15820e
syzygial <67280346+syzygial@users.noreply.github.com>
2025-06-07 17:38:01 -0400

    .

--- test/util/test_tmpfiles.cpp
+++ test/util/test_tmpfiles.cpp
@@ -5,6 +5,6 @@
 int test_util_tmpfiles() {
   ssak::util::tmpdir tmp_dir("test_tmpfiles");
   tmp_dir << fs::path("a.txt") << "Hello world\n";
-  tmp_dir << fs::path("a/b/c.txt") << "abc123\n";
+  tmp_dir << fs::path("a/b/c.txt") << "abc" << "123\n";
   return 0;
 }
