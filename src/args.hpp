9e0fffe6f3b47716fb1fca6276317b2cfbb04b24
syzygial <67280346+syzygial@users.noreply.github.com>
2025-07-17 13:52:46 -0400

    .

--- src/args.hpp
+++ src/args.hpp
@@ -4,6 +4,7 @@
 #include <map>
 #include <optional>
 #include <string>
+#include <string_view>
 #include <typeinfo>
 #include <variant>
 #include <vector>
@@ -16,27 +17,28 @@ class arg_parser {
 public:
 //using value_type = std::variant<char*,int>;
 arg_parser() {}
-void add_argument(std::string arg_name,
+void add_argument(std::optional<std::string> short_name,
     std::optional<std::string> long_name,
     std::optional<std::string> dest_name,
     value_type nargs,
     const std::type_info& arg_type) {
-  if (!this->valid_arg(arg_name, long_name, dest_name, nargs, arg_type)) return;
+  if (!this->valid_arg(short_name, long_name, dest_name, nargs, arg_type)) return;
+  if (short_name == std::nullopt) short_name = std::string();
   if (long_name == std::nullopt) long_name = std::string();
   if (dest_name == std::nullopt) {
-    if (long_name.value().empty()) dest_name = arg_name;
+    if (long_name.value().empty()) dest_name = short_name;
     else dest_name = long_name;
   }
   auto dest_start = dest_name.value().begin();
   while ((*dest_start) == '-') dest_start++;
   dest_name = std::string(dest_start, dest_name.value().end());
-  argument new_arg(arg_name, long_name.value(), dest_name.value(), nargs, arg_type);
+  argument new_arg(short_name.value(), long_name.value(), dest_name.value(), nargs, arg_type);
   this->arguments.push_back(new_arg);
 }
 std::map<key_type, value_type> parse_args(int argc, char* const* argv) {
   std::map<std::string, value_type> parsed_args;
   for (auto a : this->arguments) {
-    if (this->match(argv) == 0) continue;
+    if (this->match(a, argv) == 0) continue;
   }
   return parsed_args;
 }
@@ -57,12 +59,17 @@ struct argument {
   const std::type_info& arg_type;
 };
 unsigned int match(const argument& arg, char* const* argv) {
-  if (arg.short_name.starts_with("--"));
-  if (arg.short_name.starts_with("-"));
+  //if (arg.short_name.starts_with("--"));
+  #include <iostream>
+  if (!arg.long_name.empty() && (std::string_view(*argv)).starts_with(arg.long_name)) {
+    std::cout << ":)" << std::endl;
+  }
+  else if (arg.short_name.starts_with("-"));
   else;
   return 0;
 }
-bool valid_arg(std::string arg_name, std::optional<std::string> long_name, std::optional<std::string> dest_name, value_type nargs, const std::type_info& arg_type) {
+bool valid_arg(std::optional<std::string> short_name, std::optional<std::string> long_name, std::optional<std::string> dest_name, value_type nargs, const std::type_info& arg_type) {
+  // TODO: check validity of parameters
   return true;
 }
 std::vector<argument> arguments;
--- test/test_args.cpp
+++ test/test_args.cpp
@@ -6,5 +6,8 @@ int test_args() {
   ssak::arg_parser p;
   p.add_argument("-a", std::nullopt, std::nullopt, 0, typeid(std::string));
   p.add_argument("-t", "--test", std::nullopt, (char*)"+", typeid(int));
+  char* const argv1[] = {(char*)"--test", NULL};
+  constexpr int argc1 = sizeof(argv1) / sizeof(char*);
+  p.parse_args(argc1, argv1);
   return 0;
 }
