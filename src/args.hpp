a772bea67ce315ec628ecc8f04168c765f65d1a8
syzygial <67280346+syzygial@users.noreply.github.com>
2025-09-03 13:42:38 -0400

    .

--- src/args.hpp
+++ src/args.hpp
@@ -1,16 +1,56 @@
 #ifndef SSAK_ARGS_HPP
 #define SSAK_ARGS_HPP
 
+#include <any>
 #include <map>
 #include <optional>
 #include <stdexcept>
 #include <string>
 #include <string_view>
 #include <typeinfo>
+#include <tuple>
+#include <type_traits>
 #include <variant>
 #include <vector>
 
 namespace ssak {
+
+namespace detail {
+
+template<typename T>
+class is_vector : std::false_type {};
+
+template<typename T>
+class is_vector<std::vector<T> > : std::true_type {
+  public:
+  using vec_type = T;
+};
+
+template<typename T>
+constexpr bool is_vector_v = is_vector<T>::value;
+
+template<typename... T>
+class is_variant : std::false_type {};
+
+template<typename... T>
+class is_variant<std::variant<T...> > : std::true_type {
+  public:
+  using variant_types = std::tuple<T...>;
+};
+
+template<typename... T>
+constexpr bool is_variant_v = is_variant<T...>::value;
+
+template<typename T>
+class is_any : std::false_type {};
+
+template<>
+class is_any<std::any> : std::true_type {};
+
+template<typename T>
+constexpr bool is_any_v = is_any<T>::value;
+
+} // namespace detail
 
 class bad_argument : public std::runtime_error {
 
@@ -23,7 +63,9 @@ static constexpr char default_what[] = "Ill-formed command-line argument spec";
 };
 
 typedef enum {
-  STORE=1
+  STORE=1,
+  STORE_TRUE,
+  STORE_FALSE
 } arg_action;
 
 template
@@ -38,7 +80,8 @@ void add_argument(std::optional<std::string> short_name,
     std::optional<std::string> dest_name,
     nargs_type nargs,
     const std::type_info& arg_type,
-    bool required) {
+    bool required,
+    arg_action action) {
   if (!this->valid_arg(short_name, long_name, dest_name, nargs, arg_type)) throw bad_argument();
   if (short_name == std::nullopt) short_name = std::string();
   if (long_name == std::nullopt) long_name = std::string();
@@ -49,7 +92,7 @@ void add_argument(std::optional<std::string> short_name,
   auto dest_start = dest_name.value().begin();
   while ((*dest_start) == '-') dest_start++;
   dest_name = std::string(dest_start, dest_name.value().end());
-  argument new_arg(short_name.value(), long_name.value(), dest_name.value(), nargs, arg_type, required);
+  argument new_arg(short_name.value(), long_name.value(), dest_name.value(), nargs, arg_type, required, action);
   this->arguments.push_back(new_arg);
 }
 std::map<key_type, value_type> parse_args(int argc, char* const* argv) {
@@ -58,7 +101,7 @@ std::map<key_type, value_type> parse_args(int argc, char* const* argv) {
   for (auto& a : this->arguments) {
     unsigned int matched_args = this->match(a, argv);
     if (matched_args == 0) continue;
-    this->parse_arg(a, argv, matched_args);
+    this->parse_arg(parsed_args, a, argv, matched_args);
     //if ()
 
     argc -= matched_args;
@@ -69,20 +112,24 @@ std::map<key_type, value_type> parse_args(int argc, char* const* argv) {
 
 private:
 struct argument {
-  argument(std::string short_name, std::string long_name, std::string dest_name, nargs_type nargs, const std::type_info& arg_type, bool required) : 
+  argument(std::string short_name, std::string long_name, std::string dest_name, nargs_type nargs, const std::type_info& arg_type, bool required, arg_action action) : 
     short_name(short_name),
     long_name(long_name),
     dest_name(dest_name),
     nargs(nargs),
     arg_type(arg_type),
-    required(required) {}
+    action(action),
+    required(required) {
+      this->positional = !(this->long_name.starts_with("-"));
+    }
   std::string short_name;
   std::string long_name;
   std::string dest_name;
-  std::string dest;
   nargs_type nargs;
   const std::type_info& arg_type;
   bool required;
+  bool positional;
+  arg_action action;
 };
 unsigned int match(const argument& arg, char* const* argv) {
   //if (arg.short_name.starts_with("--"));
@@ -101,8 +148,14 @@ bool valid_arg(std::optional<std::string> short_name, std::optional<std::string>
 void initialize_args(std::map<key_type, value_type>& args) {
 
 }
-void parse_arg(const argument& matched_arg, char* const* argv, int args_matched) {
-
+void parse_arg(std::map<key_type, value_type>& args_map, const argument& matched_arg, char* const* argv, int args_matched) {
+  if (matched_arg.action == STORE_TRUE) args_map[matched_arg.dest_name] = true;
+  else if (matched_arg.action == STORE_FALSE) args_map[matched_arg.dest_name] = false;
+  else if (matched_arg.action == STORE) args_map[matched_arg.dest_name] = this->convert_arg(matched_arg, *argv);
+}
+value_type convert_arg(const argument& arg_spec, const char* arg) {
+  // TODO: write arg conversion function
+  return (char*)nullptr;
 }
 std::vector<argument> arguments;
 };
--- test/test_args.cpp
+++ test/test_args.cpp
@@ -4,8 +4,8 @@
 
 int test_args() {
   ssak::arg_parser p;
-  p.add_argument("-a", std::nullopt, std::nullopt, 0, typeid(std::string), false);
-  p.add_argument("-t", "--test", std::nullopt, (char*)"+", typeid(int), false);
+  p.add_argument("-a", std::nullopt, std::nullopt, 0, typeid(std::string), false, ssak::STORE);
+  p.add_argument("-t", "--test", std::nullopt, (char*)"+", typeid(int), false, ssak::STORE);
   char* const argv1[] = {(char*)"--test", NULL};
   constexpr int argc1 = (sizeof(argv1) / sizeof(char*)) - 1;
   p.parse_args(argc1, argv1);
